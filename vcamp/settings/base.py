eaacecb2cde818e5f75bba37ae0b77c9d522899c
Arun <arunmahara124@gmail.com>
2023-09-30 23:40:14 +0545

    fix: collect static files

--- vcamp/settings/base.py
+++ vcamp/settings/base.py
@@ -55,7 +55,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
